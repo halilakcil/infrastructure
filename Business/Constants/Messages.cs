4a07f25a772cc6d8c955ce2bd25c11c48702c95f
halilakcil <halilakcil@gmail.com>
2021-12-05 13:54:54 +0300

    feature/iauthservice + authmanager + hashinghelper

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -2,8 +2,14 @@
 {
     public static class Messages
     {
-        public static string ProductAdded => "Ürün başarıyla eklendi";
-        public static string ProductModified => "Ürün başarıyla güncellendi";
-        public static string ProductDeleted => "Ürün başarıyla silindi";
+        public static string ProductAdded = "Ürün başarıyla eklendi";
+        public static string ProductModified = "Ürün başarıyla güncellendi";
+        public static string ProductDeleted = "Ürün başarıyla silindi";
+        public static string UserNotFound = "Kullanıcı bulunamadı";
+        public static string PasswordError = "Şifre hatalı girildi";
+        public static string SuccessfulLogin = "Sisteme giriş başarılı";
+        public static string UserAlreadyExists = "Kullanıcı zaten sistemde mevcut";
+        public static string UserRegistered = "Kullanıcı başarıyla oluşturuldu";
+        public static string AccessTokenCreated = "AccessToken başarıyla oluşturuldu";
     }
 }
